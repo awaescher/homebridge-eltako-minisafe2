3d9cc05a7164c759176948629c93c4149b34f552
awaescher <awaescher@gmx.de>
2023-11-26 16:04:37 +0100

    Remove template comments

--- src/EltakoBlindsAccessory.ts
+++ src/EltakoBlindsAccessory.ts
@@ -2,11 +2,6 @@ import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
 import { EltakoMiniSafe2Platform } from './platform';
 import { IUpdatableAccessory } from './IUpdatableAccessory';
 
-/**
- * Platform Accessory
- * An instance of this class is created for each accessory your platform registers
- * Each accessory may expose multiple services of different service types.
- */
 export class EltakoBlindsAccessory implements IUpdatableAccessory {
   private service: Service;
 
@@ -15,7 +10,6 @@ export class EltakoBlindsAccessory implements IUpdatableAccessory {
     public readonly accessory: PlatformAccessory,
   ) {
 
-    // set accessory information
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
       .setCharacteristic(this.platform.Characteristic.Manufacturer, accessory.context.device.info.vendor)
       .setCharacteristic(this.platform.Characteristic.Model, accessory.context.device.info.data)
@@ -23,34 +17,27 @@ export class EltakoBlindsAccessory implements IUpdatableAccessory {
 
     const serviceType = this.platform.Service.WindowCovering;
 
-    // get the switch service if it exists, otherwise create a new switch service
-    // you can create multiple services for each accessory
     this.service = this.accessory.getService(serviceType) || this.accessory.addService(serviceType);
 
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
     this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
 
-    // register handlers for the On/Off Characteristic
     this.service.getCharacteristic(this.platform.Characteristic.CurrentPosition)
-      .onSet(this.setCurrentPosition.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getCurrentPosition.bind(this));               // GET - bind to the `getOn` method below
+      .onSet(this.setCurrentPosition.bind(this))
+      .onGet(this.getCurrentPosition.bind(this));
 
-    // register handlers for the On/Off Characteristic
     this.service.getCharacteristic(this.platform.Characteristic.TargetPosition)
-      .onSet(this.setTargetPosition.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getTargetPosition.bind(this));               // GET - bind to the `getOn` method below
+      .onSet(this.setTargetPosition.bind(this))
+      .onGet(this.getTargetPosition.bind(this));
 
-    // register handlers for the On/Off Characteristic
     this.service.getCharacteristic(this.platform.Characteristic.PositionState)
-      .onSet(this.setPositionState.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getPositionState.bind(this));               // GET - bind to the `getOn` method below
+      .onSet(this.setPositionState.bind(this))
+      .onGet(this.getPositionState.bind(this));
   }
 
   async setCurrentPosition(value: CharacteristicValue) {
     const eltakoValue = this.transformToEltako(value);
     this.platform.log.debug(`Set ${this.accessory.context.device.info.sid} Current Position ${value} (Eltako ${eltakoValue})`);
-    //await this.platform.miniSafe.setState(this.accessory.context.device.info.sid, 'on');
+    //unused: await this.platform.miniSafe.setState(this.accessory.context.device.info.sid, 'on');
   }
 
   getCurrentPosition(): CharacteristicValue {
@@ -77,14 +64,11 @@ export class EltakoBlindsAccessory implements IUpdatableAccessory {
 
   async setPositionState(value: CharacteristicValue) {
     this.platform.log.debug('Set ' + this.accessory.context.device.info.sid + ' Position State ->', value);
-    //await this.platform.miniSafe.setState(this.accessory.context.device.info.sid, 'on');
+    // unused: await this.platform.miniSafe.setState(this.accessory.context.device.info.sid, 'on');
   }
 
   getPositionState(): CharacteristicValue {
-    // Characteristic.PositionState.DECREASING	0
-    // Characteristic.PositionState.INCREASING	1
-    // Characteristic.PositionState.STOPPED	2
-    return this.platform.Characteristic.PositionState.STOPPED;
+    return this.platform.Characteristic.PositionState.STOPPED; // DECREASING 0, INCREASING 1, STOPPED 2
   }
 
   update() {
--- src/EltakoSwitchAccessory.ts
+++ src/EltakoSwitchAccessory.ts
@@ -2,11 +2,6 @@ import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
 import { EltakoMiniSafe2Platform } from './platform';
 import { IUpdatableAccessory } from './IUpdatableAccessory';
 
-/**
- * Platform Accessory
- * An instance of this class is created for each accessory your platform registers
- * Each accessory may expose multiple services of different service types.
- */
 export class EltakoSwitchAccessory implements IUpdatableAccessory {
   private service: Service;
 
@@ -15,7 +10,6 @@ export class EltakoSwitchAccessory implements IUpdatableAccessory {
     public readonly accessory: PlatformAccessory,
   ) {
 
-    // set accessory information
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
       .setCharacteristic(this.platform.Characteristic.Manufacturer, accessory.context.device.info.vendor)
       .setCharacteristic(this.platform.Characteristic.Model, accessory.context.device.info.data)
@@ -23,112 +17,26 @@ export class EltakoSwitchAccessory implements IUpdatableAccessory {
 
     const serviceType = accessory.context.device.info._target === 'light' ? this.platform.Service.Lightbulb : this.platform.Service.Switch;
 
-    // get the switch service if it exists, otherwise create a new switch service
-    // you can create multiple services for each accessory
     this.service = this.accessory.getService(serviceType) || this.accessory.addService(serviceType);
 
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
     this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.name);
 
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Switch
-
-    // register handlers for the On/Off Characteristic
     this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
-
-    // register handlers for the Brightness Characteristic
-    //this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-    //  .onSet(this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
-
-    /**
-     * Creating multiple services of the same type.
-     *
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     *
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
-
-    // Example: add two "motion sensor" services to the accessory
-    //const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-    //  this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
-
-    //const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-    //  this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
-
-    /**
-     * Updating characteristics values asynchronously.
-     *
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     *
-     */
-    // let motionDetected = false;
-    // setInterval(() => {
-    //   // EXAMPLE - inverse the trigger
-    //   motionDetected = !motionDetected;
-
-    //   // push the new value to HomeKit
-    //   motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-    //   motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
-
-    //   this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-    //   this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    // }, 10000);
+      .onSet(this.setOn.bind(this))
+      .onGet(this.getOn.bind(this));
   }
 
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
   async setOn(value: CharacteristicValue) {
-    this.platform.log.info('Set ' + this.accessory.context.device.info.sid + ' Characteristic On ->', value);
-
-    // valid commands: 'on' 'off' 'toggle'
-    await this.platform.miniSafe.sendGenericCommand(this.accessory.context.device.info.sid, value ? 'on' : 'off');
+    const command = value ? 'on' : 'off'; // valid commands: 'on' 'off' 'toggle'
+    await this.platform.miniSafe.sendGenericCommand(this.accessory.context.device.info.sid, command);
   }
 
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   *
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   *
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
-
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
   getOn(): CharacteristicValue {
-
     const state = this.platform.deviceStateCache.find(s => s.sid === this.accessory.context.device.info.sid);
-    const isOn = state?.state?.state === 'on';
-
-    return isOn;
+    return state?.state?.state === 'on';
   }
-
-  // /**
-  //  * Handle "SET" requests from HomeKit
-  //  * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-  //  */
-  // async setBrightness(value: CharacteristicValue) {
-  //   // implement your own code to set the brightness
-  //   this.exampleStates.Brightness = value as number;
-
-  //   this.platform.log.debug('Set Characteristic Brightness -> ', value);
-  // }
 
   update() {
     this.service.getCharacteristic(this.platform.Characteristic.On).updateValue(this.getOn());
   }
-
 }
